ee4afb11a2e9f455d97fc1d3a75247990344506d
Alex Lee <hyunho98@gmail.com>
2019-11-10 00:26:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(array)
+  factors = []
+
+  array.each{|i|
+    index = 1
+    while index <= i
+      factors << i if i % index == 0
+      if factors.length > 2
+        return false
+      end
+    end
+  }
+  true
+end
